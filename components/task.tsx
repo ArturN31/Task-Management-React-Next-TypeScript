c5e6fd9b10a99c3bb546b6b8a92f5c1b83b1bdeb
ArturN31 <91878136+ArturN31@users.noreply.github.com>
2023-08-27 23:03:14 +0100

    remove, overdue, styling

--- components/task.tsx
+++ components/task.tsx
@@ -1,21 +1,28 @@
 'use client'
 import { Box, Button, List, ListItem, ListItemText} from '@mui/material';
-
+import { useEffect } from 'react';
 
 export default function Task(props: any) {
   let borderColorCustom = {borderColor: '#00000044'};
 
-  let completedStyle = {}
+  let completedStyle = {};
   props.isCompleted === true 
     ? completedStyle = {
       background: 'linear-gradient(to top, white, white 15%, rgb(4 120 87))'} 
     : completedStyle = {}
+
+  let overdueStyle = {};
+  props.isOverdue === true
+    ? overdueStyle = {
+      background: 'linear-gradient(to top, white, white 15%, rgb(205 25 0))'}
+    : overdueStyle = {}
 
   type TaskProps = {
       content: string;
       isCompleted: boolean;
       due: Date | undefined;
       tags: string[];
+      isOverdue: boolean;
   }
 
   //handles removal of a task
@@ -24,7 +31,14 @@ export default function Task(props: any) {
     due: Date | undefined, 
     tags: string[]
     ) => {
-      
+      let arr = props.list.filter((task: TaskProps) => {
+        //returns all tasks beside the one that matches the passed task data effectively removing it from task list
+        if (task.content !== content 
+        && task.due !== due 
+        && task.tags !== tags) return task;
+        else return;
+      })
+      props.setList(arr);
   }
 
   //allows to mark task as completed/not completed
@@ -58,16 +72,27 @@ export default function Task(props: any) {
       props.setList(arr);
   }
 
-  //displays task header when marked as completed
-  const TaskCompleted = (isCompleted: any) => {
-    return (
-      isCompleted === true
-        ? <p 
-          className="text-center p-2 border-b shadow-sm font-medium" 
-          style={borderColorCustom}>Task completed</p>
-        : ''
-    )
+  //marks tasks as overdue when due date is in the past
+  const MarkAsOverdue = (list: TaskProps[]) => {
+    let arr = list.map((task: TaskProps) => {
+      //if task is not overdue and the task is not marked as completed
+      if (task.isOverdue !== true && task.isCompleted !== true) {
+        //if tasks due date is in the past set task as overdue
+        if (task.due && task.due < new Date()) {
+          task.isOverdue = true;
+          return task;
+        } else return task;
+      } return task;
+    })
+    props.setList(arr);
+    console.log(arr)
   }
+
+  useEffect(() => {
+    const interval = setInterval(() => {MarkAsOverdue(props.list)}, 10000);
+    return () => clearInterval(interval);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   const TaskContent = ({content, due, tags}: TaskProps) => {
     return (
@@ -112,7 +137,8 @@ export default function Task(props: any) {
   const TaskBtns = () => {
     return (
       <Box className="grid grid-flow-col-dense">
-        {props.isCompleted === true
+        {props.isCompleted === true 
+          //if completed display undo button
           ? <Button 
           onClick={() => {MarkAsCompleted(props.content, props.due, props.tags)}} 
           className="text-black" 
@@ -121,14 +147,25 @@ export default function Task(props: any) {
             borderBottom: '1px solid #00000044',
             borderRadius: '0px'
           }}>Undo</Button>
-          : <Button 
-          onClick={() => {MarkAsCompleted(props.content, props.due, props.tags)}} 
-          className="text-black" 
-          sx={{
-            borderTop: '1px solid #00000044', 
-            borderBottom: '1px solid #00000044',
-            borderRadius: '0px'
-          }}>Completed</Button>
+          : props.isOverdue !== true 
+            //if not completed and task is not overdue display completed button
+            ? <Button 
+              onClick={() => {MarkAsCompleted(props.content, props.due, props.tags)}} 
+              className="text-black" 
+              sx={{
+                borderTop: '1px solid #00000044', 
+                borderBottom: '1px solid #00000044',
+                borderRadius: '0px'
+              }}>Completed</Button>
+            //if not completed and task is overdue display disabled overdue buttone
+            : <Button
+              className="text-black"
+              disabled 
+              sx={{
+                borderTop: '1px solid #00000044', 
+                borderBottom: '1px solid #00000044',
+                borderRadius: '0px'
+              }}>Overdue</Button>
         }
         <Button 
         onClick={() => {Remove(props.content, props.due, props.tags)}}
@@ -146,19 +183,30 @@ export default function Task(props: any) {
   return (
     <Box 
     className="mt-4 mx-2 border rounded bg-white shadow-md h-fit" 
-    style={completedStyle} 
+    style={{...completedStyle, ...overdueStyle}} 
     sx={{
       borderLeft: '1px solid #00000044',
       borderRight: '1px solid #00000044',
       borderTop: '1ps solid #00000044'
     }}>
-      <TaskCompleted isCompleted={props.isCompleted}/>
+      {props.isCompleted === true
+        ? <p 
+          className="text-center p-2 border-b shadow-sm font-medium" 
+          style={borderColorCustom}>Task completed</p>
+        : ''}
+      
+      {props.isOverdue === true
+        ? <p 
+          className="text-center p-2 border-b shadow-sm font-medium" 
+          style={borderColorCustom}>Task Overdue</p>
+        : ''}
 
       <TaskContent 
       content={props.content} 
       due={props.due} 
       tags={props.tags}
-      isCompleted={props.isCompleted}/>
+      isCompleted={props.isCompleted}
+      isOverdue={props.isOverdue}/>
 
       <TaskBtns/>
     </Box>
